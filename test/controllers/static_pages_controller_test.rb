5ccad6d9e08096d0f26b192ab332a4f36eeee667
Yigal Saperstein <yigal.saperstein@macaulay.cuny.edu>
2015-06-10 16:54:04 -0400

    Finish user signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,7 +6,7 @@ class StaticPagesControllerTest < ActionController::TestCase
         @base_title = "Ruby on Rails Tutorial Sample App"
     end
     
-    test "should get home" do
+ test "should get home" do
     get :home
     assert_response :success
     assert_select "title", "#{@base_title}"
