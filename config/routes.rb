fd5a2e90091e34ad2e56c1afff5e8ffb55070be6
Yigal Saperstein <yigal.saperstein@macaulay.cuny.edu>
2015-06-17 13:12:23 -0400

    add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
     get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  resources :account_activations, only: [:edit]
     resources :users
 end
